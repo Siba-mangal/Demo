4382c172e5138439e7ac75142fc8ffea973c0eca
siba.mangal <sibaprasadmangal48@gmail.com>
2023-03-12 17:52:17 +0530

    updated

--- script.js
+++ script.js
@@ -1,75 +1,94 @@
-//single element
-// const form = document.getElementById("my-form");
-// console.log(form);
+// //single element
+// // const form = document.getElementById("my-form");
+// // console.log(form);
 
-// console.log(document.querySelector("h1"));
+// // console.log(document.querySelector("h1"));
 
-// console.log(document.querySelectorAll(".item"));
+// // console.log(document.querySelectorAll(".item"));
 
-// console.log(document.getElementsByClassName("item"));
+// // console.log(document.getElementsByClassName("item"));
 
-// const item = document.querySelectorAll(".item");
+// // const item = document.querySelectorAll(".item");
 
-// item.forEach((item) => console.log(item));
+// // item.forEach((item) => console.log(item));
 
-// const ul = document.querySelector(".items");
+// // const ul = document.querySelector(".items");
 
-// ul.firstElementChild.textContent = "Hello";
-// ul.firstElementChild.style.color = "green";
+// // ul.firstElementChild.textContent = "Hello";
+// // ul.firstElementChild.style.color = "green";
 
-// ul.children[1].innerText = "Brad";
-// ul.children[1].style.color = "yellow";
+// // ul.children[1].innerText = "Brad";
+// // ul.children[1].style.color = "yellow";
+
+// // const btn = document.querySelector(".btn");
+
+// // btn.style.background = "red";
+
+// // -----------------------------------------------------------
 
 // const btn = document.querySelector(".btn");
 
-// btn.style.background = "red";
+// btn.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   document.querySelector("#my-form").style.background = "#ccc";
+//   document.querySelector("body").classList.add("bg-dark");
+// });
 
-// -----------------------------------------------------------
+// btn.addEventListener("mouseover", (e) => {
+//   e.preventDefault();
+//   document.querySelector("#my-form").style.background = "red";
+//   document.querySelector("body").classList.add("bg-dark");
+// });
 
-const btn = document.querySelector(".btn");
+// btn.addEventListener("mouseout", (e) => {
+//   e.preventDefault();
+//   document.querySelector("#my-form").style.background = "yellow";
+//   document.querySelector("body").classList.add("bg-dark");
+// });
 
-btn.addEventListener("click", (e) => {
-  e.preventDefault();
-  document.querySelector("#my-form").style.background = "#ccc";
-  document.querySelector("body").classList.add("bg-dark");
-});
+// const myForm = document.querySelector("#my-form");
+// const nameInput = document.querySelector("#name");
+// const emailInput = document.querySelector("#email");
+// const msg = document.querySelector(".msg");
+// const userList = document.querySelector("#users");
 
-btn.addEventListener("mouseover", (e) => {
-  e.preventDefault();
-  document.querySelector("#my-form").style.background = "red";
-  document.querySelector("body").classList.add("bg-dark");
-});
+// myForm.addEventListener("submit", onSubmit);
 
-btn.addEventListener("mouseout", (e) => {
-  e.preventDefault();
-  document.querySelector("#my-form").style.background = "yellow";
-  document.querySelector("body").classList.add("bg-dark");
-});
+// function onSubmit(e) {
+//   e.preventDefault();
+//   if (nameInput.value === "" || emailInput === "") {
+//     msg.classList.add("error");
+//     msg.innerHTML = "Please enter all fields";
 
-const myForm = document.querySelector("#my-form");
-const nameInput = document.querySelector("#name");
-const emailInput = document.querySelector("#email");
-const msg = document.querySelector(".msg");
-const userList = document.querySelector("#users");
+//     setTimeout(() => msg.remove(), 3000);
+//   } else {
+//     const li = document.createElement("li");
+//     li.appendChild(
+//       document.createTextNode(`${nameInput.value} : ${emailInput.value}`)
+//     );
 
-myForm.addEventListener("submit", onSubmit);
+//     userList.appendChild(li);
 
-function onSubmit(e) {
-  e.preventDefault();
-  if (nameInput.value === "" || emailInput === "") {
-    msg.classList.add("error");
-    msg.innerHTML = "Please enter all fields";
+//     nameInput.value = "";
+//     emailInput.value = "";
+//   }
+// }
 
-    setTimeout(() => msg.remove(), 3000);
-  } else {
-    const li = document.createElement("li");
-    li.appendChild(
-      document.createTextNode(`${nameInput.value} : ${emailInput.value}`)
-    );
+// console.dir(document);
+// Examine the document object
 
-    userList.appendChild(li);
+var headerTitle = document.getElementById("header-title");
+// console.log(headerTitle);
+// console.log(headerTitle.textContent);
+// console.log(headerTitle.innerText);
 
-    nameInput.value = "";
-    emailInput.value = "";
-  }
-}
+// headerTitle.innerHTML = "<h3>Hello</h3>";
+var header = document.getElementById("main-header");
+header.style.borderBottom = "solid 3px #000";
+
+// var addItem = document.getElementsByClassName("list-group-item");
+var addItem = document.getElementsByClassName("title");
+console.log(addItem);
+
+addItem[0].style.fontWeight = "bold";
+addItem[0].style.color = "green";
