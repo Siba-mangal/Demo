f51aa9db25286533599e7149814477a9a9b90ac5
siba.mangal <sibaprasadmangal48@gmail.com>
2023-03-12 18:04:52 +0530

    updated-2

--- script.js
+++ script.js
@@ -87,8 +87,16 @@ var header = document.getElementById("main-header");
 header.style.borderBottom = "solid 3px #000";
 
 // var addItem = document.getElementsByClassName("list-group-item");
-var addItem = document.getElementsByClassName("title");
-console.log(addItem);
+// var addItem = document.getElementsByClassName("title");
+// console.log(addItem);
 
-addItem[0].style.fontWeight = "bold";
-addItem[0].style.color = "green";
+// addItem[0].style.fontWeight = "bold";
+// addItem[0].style.color = "green";
+
+var items = document.getElementsByClassName("list-group-item");
+
+items[2].style.backgroundColor = "green";
+
+for (let i = 0; i < items.length; i++) {
+  items[i].style.fontWeight = "bold";
+}
