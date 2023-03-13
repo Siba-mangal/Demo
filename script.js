4a0baaa7f76fe5f19534a3c2209d61ec837946f3
siba.mangal <sibaprasadmangal48@gmail.com>
2023-03-13 13:03:15 +0530

    finalCode

--- script.js
+++ script.js
@@ -1,155 +1,68 @@
-// //single element
-// // const form = document.getElementById("my-form");
-// // console.log(form);
+//FINAL CODE
 
-// // console.log(document.querySelector("h1"));
+// parentElement;
+const itemList = document.querySelector("#items");
+// console.log(itemList.parentElement);
+// itemList.parentElement.style.backgroundColor = "lightgreen";
+// console.log(itemList.parentElement.parentElement);
 
-// // console.log(document.querySelectorAll(".item"));
+//childNode
+// console.log(itemList.childNodes);
 
-// // console.log(document.getElementsByClassName("item"));
+// lastelementchild;
+// console.log(itemList.lastElementChild);
 
-// // const item = document.querySelectorAll(".item");
+// lastchild;
+// console.log(itemList.lastChild);
 
-// // item.forEach((item) => console.log(item));
+// childNode.createChildNode.innerHTML = "HELLO";
 
-// // const ul = document.querySelector(".items");
+// firstelementchild;
+// console.log(itemList.firstElementChild);
 
-// // ul.firstElementChild.textContent = "Hello";
-// // ul.firstElementChild.style.color = "green";
+// firstchild;
+// console.log(itemList.firstChild);
 
-// // ul.children[1].innerText = "Brad";
-// // ul.children[1].style.color = "yellow";
+// nextsibling;
+// console.log(itemList.nextSibling);
+// nextelementsibling;
+// console.log(itemList.nextElementSibling);
 
-// // const btn = document.querySelector(".btn");
+// previoussibling;
+// console.log(itemList.previousSibling);
 
-// // btn.style.background = "red";
+// previouselementsibling;
+// console.log(itemList.previousElementSibling);
 
-// // -----------------------------------------------------------
+// createelement;
+const newDiv = document.createElement("div");
+newDiv.className = "hello"; //add class
+newDiv.id = "hello1"; //add id
+newDiv.setAttribute("title", "Hello Div"); //set Atribute
 
-// const btn = document.querySelector(".btn");
+const newDivText = document.createTextNode("Hello World"); // createtesxtnode;
 
-// btn.addEventListener("click", (e) => {
-//   e.preventDefault();
-//   document.querySelector("#my-form").style.background = "#ccc";
-//   document.querySelector("body").classList.add("bg-dark");
-// });
+newDiv.appendChild(newDivText); // appendchild;
 
-// btn.addEventListener("mouseover", (e) => {
-//   e.preventDefault();
-//   document.querySelector("#my-form").style.background = "red";
-//   document.querySelector("body").classList.add("bg-dark");
-// });
+const container = document.querySelector("header .container");
 
-// btn.addEventListener("mouseout", (e) => {
-//   e.preventDefault();
-//   document.querySelector("#my-form").style.background = "yellow";
-//   document.querySelector("body").classList.add("bg-dark");
-// });
+const h1 = document.querySelector("header h1");
 
-// const myForm = document.querySelector("#my-form");
-// const nameInput = document.querySelector("#name");
-// const emailInput = document.querySelector("#email");
-// const msg = document.querySelector(".msg");
-// const userList = document.querySelector("#users");
+container.insertBefore(newDiv, h1);
 
-// myForm.addEventListener("submit", onSubmit);
+//insert before li
 
-// function onSubmit(e) {
-//   e.preventDefault();
-//   if (nameInput.value === "" || emailInput === "") {
-//     msg.classList.add("error");
-//     msg.innerHTML = "Please enter all fields";
+const newLi = document.createElement("li");
+newLi.className = "list-group-item";
 
-//     setTimeout(() => msg.remove(), 3000);
-//   } else {
-//     const li = document.createElement("li");
-//     li.appendChild(
-//       document.createTextNode(`${nameInput.value} : ${emailInput.value}`)
-//     );
+const newLiText = document.createTextNode("Hello world");
 
-//     userList.appendChild(li);
+newLi.appendChild(newLiText);
 
-//     nameInput.value = "";
-//     emailInput.value = "";
-//   }
-// }
+const container2 = document.querySelector("#items");
+const h2 = document.createElement("li");
 
-// console.dir(document);
-// Examine the document object
+const pos = container2.firstElementChild;
+container2.insertBefore(newLi, pos);
 
-// var headerTitle = document.getElementById("header-title");
-// console.log(headerTitle);
-// console.log(headerTitle.textContent);
-// console.log(headerTitle.innerText);
-
-// headerTitle.innerHTML = "<h3>Hello</h3>";
-// var header = document.getElementById("main-header");
-// header.style.borderBottom = "solid 3px #000";
-
-// var addItem = document.getElementsByClassName("list-group-item");
-// var addItem = document.getElementsByClassName("title");
-// console.log(addItem);
-
-// addItem[0].style.fontWeight = "bold";
-// addItem[0].style.color = "green";
-
-// var items = document.getElementsByClassName("list-group-item");
-
-// items[2].style.backgroundColor = "green";
-
-// for (let i = 0; i < items.length; i++) {
-//   items[i].style.fontWeight = "bold";
-// }
-
-// getElementByTagName
-
-// const li = document.getElementsByTagName("li");
-// li[1].textContent = "Hello 2";
-// li[1].style.backgroundColor = "yellow";
-
-// for (let i = 0; i < li.length; i++) {
-//   li[i].style.fontWeight = "bold";
-// }
-
-// QUERYSELECTOR
-
-// const header = document.querySelector("#main-header");
-
-// header.style.borderBottom = "solid 4px #ccc";
-
-// const input = document.querySelector("input");
-// input.value = "Hello World";
-
-// const submit = document.querySelector('input[type="submit"]');
-
-// submit.value = "SEND";
-
-// const item = document.querySelector(".list-group-item");
-
-// item.style.color = "red";
-
-// const nth_childItem = document.querySelector(".list-group-item:nth-child(2)");
-
-// nth_childItem.style.backgroundColor = "green";
-
-// document.querySelector(".list-group-item:nth-child(3)").style.display = "block";
-
-// QUERY SELECTOR ALL
-
-// const titles = document.querySelectorAll(".title");
-
-// console.log(titles);
-
-// titles[0].textContent = "Hello";
-
-const odd = document.querySelectorAll("li:nth-child(odd)");
-
-for (let i = 0; i < odd.length; i++) {
-  odd[i].style.backgroundColor = "green";
-}
-
-const second_child = document.querySelectorAll("li");
-second_child[1].style.color = "green";
-// for (let i = 0; i < second_child.length; i++) {
-//   if (i + 1 == 2) second_child[i].style.color = "green";
-// }
+console.log(newDiv);
